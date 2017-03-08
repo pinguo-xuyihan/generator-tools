01423c7c9e55b3cfd1eb13aa1fe097a3bd36c99f
pinguo-xuyihan <xuyihan@camera360.com>
2017-03-08 15:40:59 +0800

    update

--- app/templates/project/jquery/build.sh
+++ app/templates/project/jquery/build.sh
@@ -1,3 +1,7 @@
+rm -rf ./build
+rm -rf ./<%= appname %> 
+rm -rf ../../public/<%= appname %> 
+
 export NODE_PATH='/home/worker/node/lib/node_modules'
 cnpm install
 webpack --config webpack.pro.config.js
@@ -5,5 +9,9 @@ mkdir <%= appname %>
 mkdir ./build/app
 cp -r ./app/common ./build/app
 cp -r ./app/resource ./build/app
-cp -r ./build/*  ./<%= appname %>
-cp -r ./<%= appname %>  ../../public
+cp -r ./build/*  ./<%= appname %> 
+cp -r ./<%= appname %>   ../../public
+
+rm -rf ./build
+rm -rf ./<%= appname %> 
+rm -rf ./node_modules
